6ba898658f6a89e1d5ee40b9abdcad360ecf76e0
Wallace Li <hualyy@gmail.com>
2024-08-19 16:44:01 +0800

    Handle safe area

--- App.tsx
+++ App.tsx
@@ -1,16 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import MemosScreen from './src/screens/Memos';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
   return (
-    <SafeAreaView edges={['bottom', 'top']} style={{ flex: 1 }}>
-      <View style={styles.container}>
-        <StatusBar style="auto" />
-        <MemosScreen />
-      </View>
-    </SafeAreaView>
+    <SafeAreaProvider>
+      <SafeAreaView edges={['bottom', 'top']} style={{ flex: 1 }}>
+        <View style={styles.container}>
+          <StatusBar style="auto" />
+          <MemosScreen />
+        </View>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
